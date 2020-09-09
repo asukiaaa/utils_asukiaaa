3e2ffd126bdac4b2a86fde7f6087cca0d65ce50e
Asuki Kono <asukiaaa@gmail.com>
2020-09-09 13:00:46 +0900

    Separate definition to cpp file for wire

--- src/utils_asukiaaa/wire.h
+++ src/utils_asukiaaa/wire.h
@@ -6,31 +6,8 @@
 
 namespace utils_asukiaaa {
   namespace wire {
-    int readBytes(TwoWire *wire, uint8_t deviceAddress, uint8_t registerAddress, uint8_t* data, uint8_t dataLen) {
-      wire->beginTransmission(deviceAddress);
-      wire->write(registerAddress);
-      uint8_t result = wire->endTransmission();
-      if (result != 0) {
-        return result;
-      }
-
-      wire->requestFrom(deviceAddress, dataLen);
-      uint8_t index = 0;
-      while (wire->available()) {
-        uint8_t d = wire->read();
-        if (index < dataLen) {
-          data[index++] = d;
-        }
-      }
-      return 0;
-    }
-
-    int writeBytes(TwoWire *wire, uint8_t deviceAddress, uint8_t registerAddress, uint8_t* data, uint8_t dataLen) {
-      wire->beginTransmission(deviceAddress);
-      wire->write(registerAddress);
-      wire->write(data, dataLen);
-      return wire->endTransmission();
-    }
+    int readBytes(TwoWire *wire, uint8_t deviceAddress, uint8_t registerAddress, uint8_t* data, uint8_t dataLen);
+    int writeBytes(TwoWire *wire, uint8_t deviceAddress, uint8_t registerAddress, uint8_t* data, uint8_t dataLen);
   }
 }
 
