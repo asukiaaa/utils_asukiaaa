284a7cbf9962d6578e0b0182cbb451733e473d5d
Asuki Kono <asukiaaa@gmail.com>
2020-09-23 19:19:55 +0900

    Change len type for string to unsigned int

--- src/utils_asukiaaa/string.h
+++ src/utils_asukiaaa/string.h
@@ -5,12 +5,12 @@
 
 namespace utils_asukiaaa {
   namespace string {
-    String padStart(String str, int len, char pad);
-    String padNumStart(int num, int len, char pad);
-    String padNumStart(float num, int len, char pad);
-    String padEnd(String str, int len, char pad);
-    String padNumEnd(int num, int len, char pad);
-    String padNumEnd(float num, int len, char pad);
+    String padStart(String str, unsigned int len, char pad);
+    String padNumStart(int num, unsigned int len, char pad);
+    String padNumStart(float num, unsigned int len, char pad);
+    String padEnd(String str, unsigned int len, char pad);
+    String padNumEnd(int num, unsigned int len, char pad);
+    String padNumEnd(float num, unsigned int len, char pad);
   }
 }
 
